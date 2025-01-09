756a7747788cd9b2e2b2072b84ff5185e710e598
a110452 <a110452@alunos.uminho.pt>
2025-01-09 19:34:59 +0000

    Avancei estes ficheiros

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -2,6 +2,114 @@ module Desenhar where
 
 import Graphics.Gloss
 import ImmutableTowers
+import LI12425
 
-desenha :: ImmutableTowers -> Picture
-desenha _ = Translate (-450) 0 $ Scale 0.5 0.5 $ Text "Welcome to Immutable Towers!"
+-- | Estrutura principal do jogo
+data MundoJogo = MundoJogo
+   {estado :: Jogo,        -- Estado atual do jogo
+    imagemMenu :: Picture,       -- Imagem para o menu inicial
+    imagemTorreFogo :: Picture,  -- Imagem da torre de fogo
+    imagemTorreGelo :: Picture,
+    imagemTorreResina :: Picture,
+    imagemInimigo :: Picture,
+    imagemPortal :: Picture,
+    imagemBase :: Picture}   -- Imagem da torre de gelo
+
+desenha :: Jogo -> Imagens -> Picture
+desenha jogo imagens =
+  pictures
+     [desenharMapa (mapaJogo jogo),
+      desenharInimigos (inimigosJogo jogo) (inimigoPNG imagens),
+      desenharPortais (portaisJogo jogo) (portalPNG imagens),
+      desenharTorres (torresJogo jogo) (torreFogo imagens) (torreGelo imagens) (torreResina imagens),
+      desenharBase (baseJogo jogo) (basePNG imagens)]
+
+-- | Exemplo de mapa 10x10
+mapa :: Mapa
+mapa =
+  [ [a, t, a, a, r, r, a, a, a, a],
+    [a, t, a, a, a, a, a, a, a, a],
+    [r, t, r, a, a, a, a, a, a, a],
+    [r, t, r, a, r, r, a, a, a, a],
+    [r, t, r, r, r, r, r, r, r, a],
+    [t, t, t, t, t, t, t, t, t, r],
+    [r, r, a, r, t, r, r, r, t, r],
+    [r, r, a, a, t, r, r, r, t, r],
+    [r, a, a, a, t, a, a, r, t, r],
+    [a, a, a, a, t, a, a, r, t, t]
+  ]
+  where
+    t = Terra
+    r = Relva
+    a = Agua
+
+-- | Dimensões da janela
+larguraJanela, alturaJanela :: Int
+larguraJanela = 1920
+alturaJanela = 1080
+
+-- | Dimensões de cada Terreno no mapa
+larguraTerreno, alturaTerreno :: Float
+larguraTerreno = fromIntegral larguraJanela / fromIntegral (length (head mapa))
+alturaTerreno = fromIntegral alturaJanela / fromIntegral (length mapa)
+
+-- | Converter tipo de Terreno para cor
+corTerreno :: Terreno -> Color
+corTerreno Terra = makeColorI 139 69 19 255 -- Castanho para caminho
+corTerreno Relva = makeColorI 34 139 34 255 -- Verde para relva
+corTerreno Agua  = makeColorI 70 130 180 255 -- Azul para água
+
+-- | Desenhar um Terreno na sua posição
+desenharTerreno :: Terreno -> Float -> Float -> Picture
+desenharTerreno terreno x y = translate posX posY $ color (corTerreno terreno) $ rectangleSolid larguraTerreno alturaTerreno
+  where
+    posX = x * larguraTerreno - (fromIntegral larguraJanela / 2) + (larguraTerreno / 2)
+    posY = -y * alturaTerreno + (fromIntegral alturaJanela / 2) - (alturaTerreno / 2)
+
+-- | Desenhar o mapa inteiro
+desenharMapa :: Mapa -> Picture
+desenharMapa mapa =
+  pictures [desenharTerreno terreno (fromIntegral x) (fromIntegral y)
+           | (linha, y) <- zip mapa [0..], (terreno, x) <- zip linha [0..]]
+
+desenharInimigos :: [Inimigo] -> Picture -> Picture
+desenharInimigos inimigos imagemInimigo =
+  pictures [translate posX posY imagemInimigo | inimigo <- inimigos, let (posX, posY) = ajustarPosicao (posicaoInimigo inimigo)]
+  where
+    ajustarPosicao (x, y) =
+      (x * larguraTerreno - (fromIntegral larguraJanela / 2) + (larguraTerreno / 2),
+       -y * alturaTerreno + (fromIntegral alturaJanela / 2) - (alturaTerreno / 2))
+
+desenharPortais :: [Portal] -> Picture -> Picture
+desenharPortais portais imagemPortal =
+  pictures [translate posX posY imagemPortal | portal <- portais, let (posX, posY) = ajustarPosicao (posicaoPortal portal)]
+  where
+    ajustarPosicao (x, y) =
+      (x * larguraTerreno - (fromIntegral larguraJanela / 2) + (larguraTerreno / 2),
+       -y * alturaTerreno + (fromIntegral alturaJanela / 2) - (alturaTerreno / 2))
+
+desenharTorres :: [Torre] -> Picture -> Picture -> Picture -> Picture
+desenharTorres torres imagemFogo imagemGelo imagemResina =
+  pictures [desenharTorre torre | torre <- torres]
+  where
+    desenharTorre torre =
+      translate posX posY (selecionarImagem torre)
+      where
+        (posX, posY) = ajustarPosicao (posicaoTorre torre)
+        selecionarImagem t
+          | tipoProjetil (projetilTorre t) == Fogo = imagemFogo
+          | tipoProjetil (projetilTorre t) == Gelo = imagemGelo
+          | tipoProjetil (projetilTorre t) == Resina = imagemResina
+          | otherwise = blank
+    ajustarPosicao (x, y) =
+      (x * larguraTerreno - (fromIntegral larguraJanela / 2) + (larguraTerreno / 2),
+       -y * alturaTerreno + (fromIntegral alturaJanela / 2) - (alturaTerreno / 2))
+
+desenharBase :: Base -> Picture -> Picture
+desenharBase base imagemBase =
+  translate posX posY imagemBase
+  where
+    (posX, posY) = ajustarPosicao (posicaoBase base)
+    ajustarPosicao (x, y) =
+      (x * larguraTerreno - (fromIntegral larguraJanela / 2) + (larguraTerreno / 2),
+       -y * alturaTerreno + (fromIntegral alturaJanela / 2) - (alturaTerreno / 2))
--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Eventos
 import Graphics.Gloss
 import ImmutableTowers
 import Tempo
+import LI12425
 
 janela :: Display
 janela = InWindow "Immutable Towers" (1920, 1080) (0, 0)
@@ -15,11 +16,101 @@ fundo = white
 fr :: Int
 fr = 60
 
+it :: ImmutableTowers
+it = ImmutableTowers estadoInicialJogo torreDestacada imagensJogo
+
+estadoInicialJogo :: Jogo --trocar os valores
+estadoInicialJogo = Jogo
+  {baseJogo = Base
+      {vidaBase = 100.0,
+       posicaoBase = (5.0, 9.0), -- Posição inicial no mapa
+       creditosBase = 100},
+   portaisJogo = [Portal
+      {posicaoPortal = (0.0, 0.0), -- Posição do portal inicial
+       ondasPortal = [Onda {inimigosOnda = [Inimigo
+              {posicaoInimigo = (0.0, 0.0),
+               direcaoInimigo = Sul,
+               vidaInimigo = 50.0,
+               velocidadeInimigo = 1.0,
+               ataqueInimigo = 5.0,
+               butimInimigo = 10,
+               projeteisInimigo = [] }],
+               cicloOnda = 3.0,
+               tempoOnda = 3.0,
+               entradaOnda = 0.0 }] }],
+    torresJogo = [],
+    mapaJogo = inicialMapa,
+    inimigosJogo = [],
+    lojaJogo = [
+      (100, Torre
+        {posicaoTorre = (3.0, 3.0),
+         danoTorre = 20.0,
+         alcanceTorre = 5.0,
+         rajadaTorre = 1,
+         cicloTorre = 1.5,
+         tempoTorre = 0.0,
+         projetilTorre = Projetil
+            { tipoProjetil = Fogo,
+              duracaoProjetil = Finita 2.0
+            }
+        }),
+      (150, Torre
+        { posicaoTorre = (5.0, 5.0),
+          danoTorre = 10.0,
+          alcanceTorre = 4.0,
+          rajadaTorre = 1,
+          cicloTorre = 2.0,
+          tempoTorre = 0.0,
+          projetilTorre = Projetil
+            {tipoProjetil = Gelo,
+             duracaoProjetil = Finita 1.5}
+        }),
+      (50, Torre
+         {posicaoTorre = (7.0, 7.0),
+          danoTorre = 5.0,
+          alcanceTorre = 6.0,
+          rajadaTorre = 1,
+          cicloTorre = 1.0,
+          tempoTorre = 0.0,
+          projetilTorre = Projetil
+            {tipoProjetil = Resina,
+             duracaoProjetil = Continua}
+              })]
+        }
+
+torreDestacada :: Maybe Torre
+torreDestacada = Just Torre
+  { posicaoTorre = (0, 0),         -- Posição neutra (fora do mapa ou inicial)
+    danoTorre = 0,                 -- Sem dano até ser personalizada
+    alcanceTorre = 0,              -- Sem alcance inicial
+    rajadaTorre = 0,               -- Não dispara até ser configurada
+    cicloTorre = 0,                -- Sem ciclo até ser ajustada
+    tempoTorre = 0,                -- Sem tempo de espera
+    projetilTorre = Projetil
+      { tipoProjetil = Fogo,       -- Tipo inicial, pode ser alterado pelo jogador
+        duracaoProjetil = Infinita -- Duração infinita apenas como padrão
+      }
+  }
+
+-- Carrega as imagens BMP para uso no jogo
+imagensJogo :: IO [Picture]
+imagensJogo = do
+  mainMenu <- loadBMP "imagensBMP/MainMenu.bmp"
+  torreFogo <- loadBMP "imagensBMP/TorreFogo.bmp"
+  torreGelo <- loadBMP "imagensBMP/TorreGelo.bmp"
+  basePNG <- loadBMP "imagensBMP/Base.bmp"
+  return [mainMenu, torreFogo, torreGelo, basePNG]
+
+
+-- | Função principal
 main :: IO ()
 main = do
-  putStrLn "Hello from Immutable Towers!"
+  -- Carregar imagens
+  imagemMenu <- loadBMP "imagensBMP/MainMenu.bmp"
+  imagemTorreFogo <- loadBMP "imagensBMP/torrefogo.bmp"
+  imagemTorreGelo <- loadBMP "imagensBMP/torregelo.bmp"
+  imagemBase <- loadBMP "imagensBMP/Base.bmp"
+  play janela fundo fr it desenhaJogo reageEventos reageTempo
+    where it = ImmutableTowers {}
 
-  play janela fundo fr it desenha reageEventos reageTempo
-  where
-    it = ImmutableTowers {}
 
