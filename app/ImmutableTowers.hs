7d1b326a002919bd00dcdf4411b4cd9857303f6e
a110452 <a110452@alunos.uminho.pt>
2025-01-11 02:44:00 +0000

    Adaptada após revisão da Desenhar.hs

--- app/ImmutableTowers.hs
+++ app/ImmutableTowers.hs
@@ -4,15 +4,6 @@ import Graphics.Gloss
 import LI12425
 
 data ImmutableTowers = ImmutableTowers
-   {estadoJogo :: Jogo, 
-    torreDestacada :: Maybe Torre,
-    imagens :: Imagens}
-
-data Imagens = ImagensJogo
-  {mainMenu :: Picture,
-  portalPNG :: Picture,
-  inimigoPNG :: Picture, 
-  torreResina :: Picture,
-  torreFogo :: Picture, 
-  torreGelo :: Picture, 
-  basePNG :: Picture}
+   {creditos :: Creditos,
+   torreSelecionada :: Maybe Torre,
+   mapa :: Mapa}
--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,8 @@ import Desenhar
 import Eventos
 import Graphics.Gloss
 import ImmutableTowers
-import Tempo
 import LI12425
+import Tempo
 
 janela :: Display
 janela = InWindow "Immutable Towers" (1920, 1080) (0, 0)
@@ -17,23 +17,23 @@ fr :: Int
 fr = 60
 
 it :: ImmutableTowers
-it = ImmutableTowers estadoInicialJogo torreDestacada imagensJogo
+it = ImmutableTowers estadoInicialJogo Nothing imagensJogo
 
 estadoInicialJogo :: Jogo --trocar os valores
 estadoInicialJogo = Jogo
   {baseJogo = Base
-      {vidaBase = 100.0,
-       posicaoBase = (0.0, 0.0), -- Posição inicial no mapa
-       creditosBase = 100},
+      {vidaBase = 1000.0,
+       posicaoBase = (5, 5), -- Posição inicial no mapa
+       creditosBase = 75},
    portaisJogo = [Portal
-      {posicaoPortal = (0.0, 0.0), -- Posição do portal inicial
+      {posicaoPortal = (7, 3), -- Posição do portal inicial
        ondasPortal = [Onda {inimigosOnda = [Inimigo
               {posicaoInimigo = (0.0, 0.0),
                direcaoInimigo = Sul,
                vidaInimigo = 50.0,
                velocidadeInimigo = 1.0,
                ataqueInimigo = 5.0,
-               butimInimigo = 10,
+               butimInimigo = 25,
                projeteisInimigo = [] }],
                cicloOnda = 3.0,
                tempoOnda = 3.0,
@@ -42,70 +42,32 @@ estadoInicialJogo = Jogo
     mapaJogo = inicialMapa,
     inimigosJogo = [],
     lojaJogo = [
-      (100, Torre
-        {posicaoTorre = (0.0, 0.0),
-         danoTorre = 20.0,
-         alcanceTorre = 5.0,
-         rajadaTorre = 1,
-         cicloTorre = 1.5,
-         tempoTorre = 0.0,
-         projetilTorre = Projetil
-             {tipoProjetil = Fogo,
-              duracaoProjetil = Finita 2.0} }),
-      (150, Torre
-         {posicaoTorre = (0.0, 0.0),
-          danoTorre = 10.0,
-          alcanceTorre = 4.0,
-          rajadaTorre = 1,
-          cicloTorre = 2.0,
-          tempoTorre = 0.0,
-          projetilTorre = Projetil
-            {tipoProjetil = Gelo,
-             duracaoProjetil = Finita 1.5} }),
-      (50, Torre
-         {posicaoTorre = (0.0, 0.0),
-          danoTorre = 5.0,
-          alcanceTorre = 6.0,
-          rajadaTorre = 1,
-          cicloTorre = 1.0,
-          tempoTorre = 0.0,
-          projetilTorre = Projetil
-            {tipoProjetil = Resina,
-             duracaoProjetil = Continua} })] }
+          (150, Torre {posicaoTorre = (0.0, 0.0), danoTorre = 20.0, alcanceTorre = 5.0,
+                      rajadaTorre = 1, cicloTorre = 1.5, tempoTorre = 0.0,
+                      projetilTorre = Projetil {tipoProjetil = Fogo, duracaoProjetil = Finita 2.0}}),
+          (300, Torre {posicaoTorre = (0.0, 0.0), danoTorre = 10.0, alcanceTorre = 4.0,
+                      rajadaTorre = 1, cicloTorre = 2.0, tempoTorre = 0.0,
+                      projetilTorre = Projetil {tipoProjetil = Gelo, duracaoProjetil = Finita 1.5}}),
+          (75, Torre {posicaoTorre = (0.0, 0.0), danoTorre = 5.0, alcanceTorre = 6.0,
+                      rajadaTorre = 1, cicloTorre = 1.0, tempoTorre = 0.0,
+                      projetilTorre = Projetil {tipoProjetil = Resina, duracaoProjetil = Continua}})]}
 
-torreDestacada :: Maybe Torre
-torreDestacada = Just Torre
-   {posicaoTorre = (0, 0),         -- Posição neutra (fora do mapa ou inicial)
-    danoTorre = 0,                 -- Sem dano até ser personalizada
-    alcanceTorre = 0,              -- Sem alcance inicial
-    rajadaTorre = 0,               -- Não dispara até ser configurada
-    cicloTorre = 0,                -- Sem ciclo até ser ajustada
-    tempoTorre = 0,                -- Sem tempo de espera
-    projetilTorre = Projetil
-       {tipoProjetil = Fogo,       -- Tipo inicial, pode ser alterado pelo jogador
-        duracaoProjetil = Infinita}} -- Duração infinita apenas como padrão
-
--- Carrega as imagens BMP para uso no jogo
-imagensJogo :: IO [Picture]
-imagensJogo = do
-  mainMenu <- loadBMP "imagensBMP/MainMenu.bmp"
-  torreFogo <- loadBMP "imagensBMP/TorreFogo.bmp"
-  torreGelo <- loadBMP "imagensBMP/TorreGelo.bmp"
-  basePNG <- loadBMP "imagensBMP/Base.bmp"
-  return [mainMenu, torreFogo, torreGelo, basePNG]
-
-
--- | Função principal
 main :: IO ()
 main = do
-  -- Carregar imagens
-  imagemMenu <- loadBMP "imagensBMP/MainMenu.bmp"
-  imagemTorreFogo <- loadBMP "imagensBMP/torrefogo.bmp"
-  imagemTorreGelo <- loadBMP "imagensBMP/torregelo.bmp"
-  imagemBase <- loadBMP "imagensBMP/Base.bmp"
-  play janela fundo fr it desenhaJogo reageEventos reageTempo
-    where it = ImmutableTowers {}
+  let imagemMapa = translate (-(fromIntegral largura / 1.45)) (fromIntegral altura / 1.48) (desenhaMapa mapa)
+  -- Carrega as imagens
+  imagemBase <- desenhaBase "imagensBMP/BaseD.bmp" 5 5  
+  imagemVida <- desenhaVida "imagensBMP/HPui.bmp" 16 (-3)
+  imagemMoeda <- desenhaMoeda "imagensBMP/CreditosBMP.bmp" 14 (0)
+  imagemTabua <- desenhaTabua "imagensBMP/woodenplank.bmp" 17 (0)
+  imagemPortal1 <- desenhaPortal "imagensBMP/PortalBMP.bmp" 7 3
+  imagemPortal2 <- desenhaPortal "imagensBMP/PortalBMP.bmp" 10 12
 
+  -- Carrega o produto final
+  uiAcabada <- desenhaLoja "imagensBMP/TorreFogoLoja.bmp"  "imagensBMP/TorreGeloLoja.bmp" 
+                           (pictures [imagemMapa, imagemBase, imagemPortal1, imagemPortal2, imagemVida, imagemMoeda, imagemTabua])
+
+  play janela fundo fr it (return uiAcabada) reageEventos reageTempo
 
 
 
