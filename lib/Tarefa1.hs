5f19c0e11a4d89c80cd4fdb944967674c7ad9bb6
José <a110367@alunos.uminho.pt>
2025-01-13 02:30:41 +0000

    Haddock feito Tarefa1 e Tarefa2

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -10,36 +10,63 @@ module Tarefa1 where
 
 import LI12425
 
--- Valida se um estado de jogo é válido
+{-|
+Valida se um estado de jogo é válido.
+
+### Exemplos:
+>>> validaJogo Jogo { portaisJogo = [], baseJogo = Base { posicaoBase = (0,0), creditosBase = 0 }, mapaJogo = [], torresJogo = [], inimigosJogo = [] }
+False
+>>> validaJogo Jogo { portaisJogo = [Portal {posicaoPortal = (1,1), ondasPortal = []}], baseJogo = Base { posicaoBase = (0,0), creditosBase = 10 }, mapaJogo = [[Terra]], torresJogo = [], inimigosJogo = [] }
+True
+-}
 validaJogo :: Jogo -> Bool
 validaJogo Jogo{portaisJogo = [], baseJogo = Base{posicaoBase = _}} = False
 validaJogo jogo@(Jogo{baseJogo = base, portaisJogo = portais, mapaJogo = mapa, torresJogo = torres}) =
-  -- Verifica se existem portais, e se todos estão válidos
   not (null portais) && all (\portal -> validaPortal portal mapa (posicaoBase base) torres) portais &&
-  -- Verifica se todos os inimigos por lançar e em jogo são válidos
   validaInimigosPorLancar (inimigosJogo jogo) portais &&
   validaInimigosEmJogo (inimigosJogo jogo) mapa (posicaoBase base) torres &&
-  -- Verifica se todas as torres são válidas
   all (validaTorre mapa torres) torres &&
-  -- Verifica se a base é válida
   validaBase base mapa torres portais
 
--- Valida o portal considerando as regras fornecidas
+{-|
+Valida o portal considerando as regras fornecidas.
+
+### Exemplos:
+>>> validaPortal Portal { posicaoPortal = (1,1), ondasPortal = [] } [[Terra]] (0,0) []
+True
+>>> validaPortal Portal { posicaoPortal = (1,1), ondasPortal = [] } [[Relva]] (0,0) []
+False
+-}
 validaPortal :: Portal -> Mapa -> Posicao -> [Torre] -> Bool
 validaPortal Portal{posicaoPortal = posPortal, ondasPortal = ondas} mapa basePos torres =
-  eTerra posPortal mapa &&                              -- (1.b)
-  validaCaminhoTerra posPortal mapa basePos &&           -- (1.c)
-  not (posicaoOcupada posPortal basePos torres) &&      -- (1.d)
-  validaOndas ondas && -- (1.e) Verifica as ondas
-  length ondas <= 1  -- Máximo uma onda ativa por portal
+  eTerra posPortal mapa &&
+  validaCaminhoTerra posPortal mapa basePos &&
+  not (posicaoOcupada posPortal basePos torres) &&
+  validaOndas ondas &&
+  length ondas <= 1
 
+{-|
+Verifica se a posição está ocupada pela base ou torres.
 
--- Verifica se a posição está ocupada pela base ou torres
+### Exemplos:
+>>> posicaoOcupada (0,0) (0,0) []
+True
+>>> posicaoOcupada (1,1) (0,0) [Torre { posicaoTorre = (1,1), alcanceTorre = 2, rajadaTorre = 1, cicloTorre = 3 }]
+True
+-}
 posicaoOcupada :: Posicao -> Posicao -> [Torre] -> Bool
 posicaoOcupada portalPos basePos torres =
   portalPos == basePos || any (\Torre{posicaoTorre = posTorre} -> portalPos == posTorre) torres
 
--- Valida se há um caminho de terra entre o portal e a base
+{-|
+Valida se há um caminho de terra entre o portal e a base.
+
+### Exemplos:
+>>> validaCaminhoTerra (0,0) [[Terra]] (0,0)
+True
+>>> validaCaminhoTerra (0,1) [[Relva]] (0,0)
+False
+-}
 validaCaminhoTerra :: Posicao -> Mapa -> Posicao -> Bool
 validaCaminhoTerra (x, y) mapa b =
   eTerra (x, y) mapa &&
@@ -48,28 +75,60 @@ validaCaminhoTerra (x, y) mapa b =
    caminhoTerra (x, y-1) mapa b ||
    caminhoTerra (x, y+1) mapa b)
 
--- Valida o caminho de terra recursivamente
+{-|
+Valida o caminho de terra recursivamente.
+
+### Exemplos:
+>>> caminhoTerra (0,0) [[Terra]] (0,0)
+True
+>>> caminhoTerra (1,1) [[Terra, Relva], [Relva, Relva]] (0,0)
+False
+-}
 caminhoTerra :: Posicao -> Mapa -> Posicao -> Bool
 caminhoTerra p@(x, y) mapa b
   | p == b  = True
   | not (eTerra p mapa) = False
   | otherwise = caminhoTerra (x-1, y) mapa b || caminhoTerra (x+1, y) mapa b || caminhoTerra (x, y-1) mapa b || caminhoTerra (x, y+1) mapa b
 
--- Verifica se a posição do mapa é "Terra"
+{-|
+Verifica se a posição do mapa é "Terra".
+
+### Exemplos:
+>>> eTerra (0,0) [[Terra]]
+True
+>>> eTerra (1,1) [[Terra, Relva], [Relva, Relva]]
+False
+-}
 eTerra :: Posicao -> Mapa -> Bool
 eTerra (x, y) mapa =
   let x' = floor x
       y' = floor y
   in y' >= 0 && y' < length mapa && x' >= 0 && x' < length (mapa !! y') && (mapa !! y' !! x') == Terra
 
--- Verifica se a posição do mapa é "Relva"
+{-|
+Verifica se a posição do mapa é "Relva".
+
+### Exemplos:
+>>> eRelva (0,0) [[Relva]]
+True
+>>> eRelva (1,1) [[Relva, Terra], [Terra, Terra]]
+False
+-}
 eRelva :: Posicao -> Mapa -> Bool
 eRelva (x, y) mapa =
   let x' = floor x
       y' = floor y
   in y' >= 0 && y' < length mapa && x' >= 0 && x' < length (mapa !! y') && (mapa !! y' !! x') == Relva
 
--- Valida se a lista de projéteis ativos está "normalizada"
+{-|
+Valida se a lista de projéteis ativos está "normalizada".
+
+### Exemplos:
+>>> validaProjeteisAtivos [Projetil { tipoProjetil = Fogo }]
+True
+>>> validaProjeteisAtivos [Projetil { tipoProjetil = Fogo }, Projetil { tipoProjetil = Fogo }]
+False
+-}
 validaProjeteisAtivos :: [Projetil] -> Bool
 validaProjeteisAtivos projeteis =
   not (temDuplicados projeteis) &&
@@ -88,11 +147,27 @@ validaProjeteisAtivos projeteis =
     temTipo :: TipoProjetil -> [Projetil] -> Bool
     temTipo t = any (\p -> tipoProjetil p == t)
 
--- Valida se todos os inimigos por lançar cumprem os critérios
+{-|
+Valida se todos os inimigos por lançar cumprem os critérios.
+
+### Exemplos:
+>>> validaInimigosPorLancar [] []
+True
+>>> validaInimigosPorLancar [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] [Portal { posicaoPortal = (1,1), ondasPortal = [] }]
+True
+-}
 validaInimigosPorLancar :: [Inimigo] -> [Portal] -> Bool
 validaInimigosPorLancar inimigos portais = all (\inimigo -> validaInimigoPorLancar inimigo portais) inimigos
 
--- Valida se um inimigo por lançar cumpre os critérios
+{-|
+Valida se um inimigo por lançar cumpre os critérios.
+
+### Exemplos:
+>>> validaInimigoPorLancar Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 } [Portal { posicaoPortal = (1,1), ondasPortal = [] }]
+True
+>>> validaInimigoPorLancar Inimigo { posicaoInimigo = (1,2), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 } [Portal { posicaoPortal = (1,1), ondasPortal = [] }]
+False
+-}
 validaInimigoPorLancar :: Inimigo -> [Portal] -> Bool
 validaInimigoPorLancar Inimigo{posicaoInimigo = pos, vidaInimigo = vida, projeteisInimigo = projeteis, velocidadeInimigo = vel} portais =
   any (\Portal{posicaoPortal = posPortal} -> pos == posPortal) portais &&
@@ -101,12 +176,28 @@ validaInimigoPorLancar Inimigo{posicaoInimigo = pos, vidaInimigo = vida, projete
   vel >= 0 &&
   validaProjeteisAtivos projeteis
 
+{-|
+Valida se todos os inimigos em jogo cumprem os critérios.
 
--- Valida se todos os inimigos em jogo cumprem os critérios
+### Exemplos:
+>>> validaInimigosEmJogo [] [[Terra]] (0,0) []
+True
+>>> validaInimigosEmJogo [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] [[Terra]] (0,0) []
+True
+-}
+
 validaInimigosEmJogo :: [Inimigo] -> Mapa -> Posicao -> [Torre] -> Bool
 validaInimigosEmJogo inimigos mapa basePos torres = all (\inimigo -> validaInimigoEmJogo inimigo mapa basePos torres) inimigos
 
--- Valida se um inimigo em jogo cumpre os critérios
+{-|
+Valida se um inimigo em jogo cumpre os critérios.
+
+### Exemplos:
+>>> validaInimigoEmJogo Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 } [[Terra]] (0,0) []
+True
+>>> validaInimigoEmJogo Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = -1 } [[Terra]] (0,0) []
+False
+-}
 validaInimigoEmJogo :: Inimigo -> Mapa -> Posicao -> [Torre] -> Bool
 validaInimigoEmJogo Inimigo{posicaoInimigo = pos, velocidadeInimigo = vel, projeteisInimigo = projeteis} mapa basePos torres =
   eTerra pos mapa && 
@@ -114,7 +205,15 @@ validaInimigoEmJogo Inimigo{posicaoInimigo = pos, velocidadeInimigo = vel, proje
   vel >= 0 &&
   validaProjeteisAtivos projeteis
 
--- Valida se uma torre cumpre os critérios
+{-|
+Valida se uma torre cumpre os critérios.
+
+### Exemplos:
+>>> validaTorre [[Relva]] [] Torre { posicaoTorre = (0,0), alcanceTorre = 1, rajadaTorre = 1, cicloTorre = 2 }
+True
+>>> validaTorre [[Terra]] [] Torre { posicaoTorre = (0,0), alcanceTorre = 1, rajadaTorre = 1, cicloTorre = 2 }
+False
+-}
 validaTorre :: Mapa -> [Torre] -> Torre -> Bool
 validaTorre mapa torres Torre{posicaoTorre = pos, alcanceTorre = alcance, rajadaTorre = rajada, cicloTorre = ciclo} =
   eRelva pos mapa &&  -- (3.a)
@@ -123,11 +222,30 @@ validaTorre mapa torres Torre{posicaoTorre = pos, alcanceTorre = alcance, rajada
   ciclo >= 0 &&       -- (3.d)
   not (torreSobreposta pos torres)  -- (3.e)
 
--- Verifica se uma torre está sobreposta a outra
+
+{-|
+Verifica se uma torre está sobreposta a outra.
+
+### Exemplos:
+>>> torreSobreposta (0,0) [Torre { posicaoTorre = (0,0), alcanceTorre = 2, rajadaTorre = 1, cicloTorre = 3 }]
+True
+>>> torreSobreposta (1,1) [Torre { posicaoTorre = (0,0), alcanceTorre = 2, rajadaTorre = 1, cicloTorre = 3 }]
+False
+-}
 torreSobreposta :: Posicao -> [Torre] -> Bool
 torreSobreposta pos torres = any (\Torre{posicaoTorre = posTorre} -> pos == posTorre) torres
 
--- Valida se a base cumpre os critérios
+
+{-|
+Valida se a base cumpre os critérios.
+
+### Exemplos:
+>>> validaBase Base { posicaoBase = (0,0), creditosBase = 10 } [[Terra]] [] []
+True
+>>> validaBase Base { posicaoBase = (0,0), creditosBase = -1 } [[Terra]] [] []
+False
+-}
+
 validaBase :: Base -> Mapa -> [Torre] -> [Portal] -> Bool
 validaBase Base{posicaoBase = pos, creditosBase = credito} mapa torres portais =
   eTerra pos mapa &&  -- (4.a)
@@ -135,13 +253,32 @@ validaBase Base{posicaoBase = pos, creditosBase = credito} mapa torres portais =
   not (posicaoOcupada pos pos torres) &&  -- (4.c)
   not (any (\Portal{posicaoPortal = posPortal} -> pos == posPortal) portais)
 
--- Valida se as ondas de um portal são válidas
+
+{-|
+Valida se as ondas de um portal são válidas.
+
+### Exemplos:
+>>> validaOndas [Onda { inimigosOnda = [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] }]
+True
+>>> validaOndas [Onda { inimigosOnda = [] }]
+False
+-}
+
 validaOndas :: [Onda] -> Bool
 validaOndas = all validaOnda
 
--- Valida se uma onda é válida
+ {-|
+Valida se uma onda é válida.
+
+### Exemplos:
+>>> validaOnda Onda { inimigosOnda = [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] }
+True
+>>> validaOnda Onda { inimigosOnda = [] }
+False
+-}
+
 validaOnda :: Onda -> Bool
 validaOnda Onda{inimigosOnda = inimigos} = not (null inimigos)
 
---tarefa1 concluída--
+  --tarefa1 concluída--
 
--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -1,81 +1,118 @@
-  {-|
-  Module      : Tarefa2
-  Description : Auxiliares do Jogo
-  Copyright   : Jose dos Santos Gomes <a110367@alunos.uminho.pt>
-                Guilherme Figueiredo Castro <a110452@alunos.uminho.pt>
+{-|
+Module      : Tarefa2
+Description : Auxiliares do Jogo
+Copyright   : Jose dos Santos Gomes <a110367@alunos.uminho.pt>
+              Guilherme Figueiredo Castro <a110452@alunos.uminho.pt>
 
-  Módulo para a realização da Tarefa 2 de LI1 em 2024/25.
-  -}
-  module Tarefa2 where
+Módulo para a realização da Tarefa 2 de LI1 em 2024/25.
+-}
+module Tarefa2 where
 
-  import LI12425
+import LI12425
 
-  -- | A função 'inimigosNoAlcance' recebe uma torre e uma lista de inimigos e retorna uma lista dos inimigos que estão ao alcance da torre.
-  -- (Alínea 1)
-  inimigosNoAlcance :: Torre -> [Inimigo] -> [Inimigo]
-  inimigosNoAlcance torre = filter (\inimigo -> distancia (posicaoInimigo inimigo) (posicaoTorre torre) <= alcanceTorre torre)
-  
-  distancia :: Posicao -> Posicao -> Float
-  distancia (x1, y1) (x2, y2) = sqrt ((x1 - x2) ^ (2 :: Integer) + (y1 - y2) ^ (2 :: Integer))
+{-|
+A função 'inimigosNoAlcance' recebe uma torre e uma lista de inimigos e retorna uma lista dos inimigos que estão ao alcance da torre.
 
-  -- | 'atingeInimigo' aplica o dano de uma torre a um inimigo, reduzindo a vida do inimigo.
-  --   Também atualiza a lista de projéteis do inimigo com base nas sinergias entre os projéteis.
-  -- (Alínea 2)
-  atingeInimigo :: Torre -> Inimigo -> Inimigo
-  atingeInimigo Torre {danoTorre = dano, projetilTorre = projTorre} inimigo@Inimigo {vidaInimigo = vida, projeteisInimigo = projInimigo} =
-    inimigo { vidaInimigo = max 0 (vida - dano), projeteisInimigo = atualizaProjeteis projTorre projInimigo}
-    where
-      -- Funções de sinergia de projéteis
-      atualizaProjeteis :: Projetil -> [Projetil] -> [Projetil]
-      atualizaProjeteis proj [] = [proj]
-      atualizaProjeteis proj (p:ps)
-        | cancelaMutuamente proj p = atualizaProjeteis proj ps
-        | dobraDuracao proj p = proj {duracaoProjetil = dobra proj} : atualizaProjeteis proj ps
-        | dobraDuracao' proj p = p {duracaoProjetil = dobra p} : atualizaProjeteis p ps
-        | otherwise = p : atualizaProjeteis proj ps
+### Exemplos:
+>>> inimigosNoAlcance Torre { posicaoTorre = (0, 0), alcanceTorre = 2 } [Inimigo { posicaoInimigo = (1, 1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }]
+[Inimigo {posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1}]
+>>> inimigosNoAlcance Torre { posicaoTorre = (0, 0), alcanceTorre = 1 } [Inimigo { posicaoInimigo = (2, 2), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }]
+[]
+-}
+inimigosNoAlcance :: Torre -> [Inimigo] -> [Inimigo]
+inimigosNoAlcance torre = filter (\inimigo -> distancia (posicaoInimigo inimigo) (posicaoTorre torre) <= alcanceTorre torre)
 
-      -- Cancelamentos mutuamente
-      cancelaMutuamente :: Projetil -> Projetil -> Bool
-      cancelaMutuamente (Projetil Gelo _) (Projetil Fogo _) = True
-      cancelaMutuamente (Projetil Fogo _) (Projetil Gelo _) = True
-      cancelaMutuamente _ _ = False
+distancia :: Posicao -> Posicao -> Float
+distancia (x1, y1) (x2, y2) = sqrt ((x1 - x2) ^ (2 :: Integer) + (y1 - y2) ^ (2 :: Integer))
 
-      -- Dobra a duração de projéteis
-      dobraDuracao :: Projetil -> Projetil -> Bool
-      dobraDuracao (Projetil Fogo _) (Projetil Resina _) = True
-      dobraDuracao _ _ = False
+{-|
+Aplica o dano de uma torre a um inimigo, reduzindo sua vida e atualizando os projéteis.
 
-      dobraDuracao' :: Projetil -> Projetil -> Bool
-      dobraDuracao' (Projetil Resina _) (Projetil Fogo _) = True
-      dobraDuracao' _ _ = False
+### Exemplos:
+>>> atingeInimigo Torre { danoTorre = 5, projetilTorre = Projetil Fogo (Finita 3) } Inimigo { vidaInimigo = 10, projeteisInimigo = [], posicaoInimigo = (1,1), velocidadeInimigo = 1 }
+Inimigo {posicaoInimigo = (1,1), vidaInimigo = 5, projeteisInimigo = [Projetil Fogo (Finita 3)], velocidadeInimigo = 1}
+>>> atingeInimigo Torre { danoTorre = 15, projetilTorre = Projetil Fogo (Finita 3) } Inimigo { vidaInimigo = 10, projeteisInimigo = [], posicaoInimigo = (1,1), velocidadeInimigo = 1 }
+Inimigo {posicaoInimigo = (1,1), vidaInimigo = 0, projeteisInimigo = [Projetil Fogo (Finita 3)], velocidadeInimigo = 1}
+-}
+atingeInimigo :: Torre -> Inimigo -> Inimigo
+atingeInimigo Torre {danoTorre = dano, projetilTorre = projTorre} inimigo@Inimigo {vidaInimigo = vida, projeteisInimigo = projInimigo} =
+  inimigo { vidaInimigo = max 0 (vida - dano), projeteisInimigo = atualizaProjeteis projTorre projInimigo }
+  where
+    atualizaProjeteis :: Projetil -> [Projetil] -> [Projetil]
+    atualizaProjeteis proj [] = [proj]
+    atualizaProjeteis proj (p:ps)
+      | cancelaMutuamente proj p = atualizaProjeteis proj ps
+      | dobraDuracao proj p = proj {duracaoProjetil = dobra proj} : atualizaProjeteis proj ps
+      | dobraDuracao' proj p = p {duracaoProjetil = dobra p} : atualizaProjeteis p ps
+      | otherwise = p : atualizaProjeteis proj ps
 
-      dobra :: Projetil -> Duracao
-      dobra (Projetil _ (Finita t)) = Finita (2 * t)
-      dobra (Projetil _ Infinita) = Infinita
+    cancelaMutuamente :: Projetil -> Projetil -> Bool
+    cancelaMutuamente (Projetil Gelo _) (Projetil Fogo _) = True
+    cancelaMutuamente (Projetil Fogo _) (Projetil Gelo _) = True
+    cancelaMutuamente _ _ = False
 
+    dobraDuracao :: Projetil -> Projetil -> Bool
+    dobraDuracao (Projetil Fogo _) (Projetil Resina _) = True
+    dobraDuracao _ _ = False
 
-  -- | 'terminouJogo' verifica se o jogo terminou, seja por vitória ou derrota.
-  -- (Alínea 3)
-  terminouJogo :: Jogo -> Bool
-  terminouJogo jogo = ganhouJogo jogo || perdeuJogo jogo
+    dobraDuracao' :: Projetil -> Projetil -> Bool
+    dobraDuracao' (Projetil Resina _) (Projetil Fogo _) = True
+    dobraDuracao' _ _ = False
 
-  -- | 'ganhouJogo' verifica se o jogador ganhou o jogo, ou seja, não há mais inimigos e a vida da base é maior que 0. (win condition)
-  ganhouJogo :: Jogo -> Bool
-  ganhouJogo Jogo {baseJogo = Base {vidaBase = vida}, inimigosJogo = inimigos} = vida > 0 && null inimigos
+    dobra :: Projetil -> Duracao
+    dobra (Projetil _ (Finita t)) = Finita (2 * t)
+    dobra (Projetil _ Infinita) = Infinita
 
-  -- | 'perdeuJogo' verifica se o jogador perdeu o jogo, ou seja, a vida da base é 0 ou menor. (lose condition)
-  perdeuJogo :: Jogo -> Bool
-  perdeuJogo Jogo {baseJogo = Base {vidaBase = vida}} = vida <= 0
+{-|
+Verifica se o jogo terminou.
 
-  -- | 'ativaInimigo' ativa o próximo inimigo na onda de um portal e atualiza o estado do jogo.
-  -- (Alínea 4)
-  ativaInimigo :: Portal -> Jogo -> Jogo 
-  ativaInimigo Portal {ondasPortal = []} jogo = jogo
-  ativaInimigo portal@Portal {ondasPortal = (onda:ondas)} jogo = 
-    jogo { inimigosJogo = inimigosJogo jogo ++ novosInimigos, portaisJogo = atualizaPortal portal } 
-    where 
+### Exemplos:
+>>> terminouJogo Jogo { baseJogo = Base { vidaBase = 0 }, inimigosJogo = [] }
+True
+>>> terminouJogo Jogo { baseJogo = Base { vidaBase = 10 }, inimigosJogo = [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] }
+False
+-}
+terminouJogo :: Jogo -> Bool
+terminouJogo jogo = ganhouJogo jogo || perdeuJogo jogo
+
+{-|
+Verifica se o jogador ganhou o jogo (condição de vitória).
+
+### Exemplos:
+>>> ganhouJogo Jogo { baseJogo = Base { vidaBase = 10 }, inimigosJogo = [] }
+True
+>>> ganhouJogo Jogo { baseJogo = Base { vidaBase = 10 }, inimigosJogo = [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] }
+False
+-}
+ganhouJogo :: Jogo -> Bool
+ganhouJogo Jogo {baseJogo = Base {vidaBase = vida}, inimigosJogo = inimigos} = vida > 0 && null inimigos
+
+{-|
+Verifica se o jogador perdeu o jogo (condição de derrota).
+
+### Exemplos:
+>>> perdeuJogo Jogo { baseJogo = Base { vidaBase = 0 } }
+True
+>>> perdeuJogo Jogo { baseJogo = Base { vidaBase = 10 } }
+False
+-}
+perdeuJogo :: Jogo -> Bool
+perdeuJogo Jogo {baseJogo = Base {vidaBase = vida}} = vida <= 0
+
+{-|
+Ativa o próximo inimigo na onda de um portal.
+
+### Exemplos:
+>>> ativaInimigo Portal { ondasPortal = [Onda { inimigosOnda = [Inimigo { posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1 }] }] } Jogo { inimigosJogo = [], portaisJogo = [] }
+Jogo {inimigosJogo = [Inimigo {posicaoInimigo = (1,1), vidaInimigo = 10, projeteisInimigo = [], velocidadeInimigo = 1}], portaisJogo = [Portal {ondasPortal = [Onda {inimigosOnda = []}]}]}
+>>> ativaInimigo Portal { ondasPortal = [] } Jogo { inimigosJogo = [], portaisJogo = [] }
+Jogo {inimigosJogo = [], portaisJogo = []}
+-}
+ativaInimigo :: Portal -> Jogo -> Jogo 
+ativaInimigo Portal {ondasPortal = []} jogo = jogo
+ativaInimigo portal@Portal {ondasPortal = (onda:ondas)} jogo = 
+  jogo { inimigosJogo = inimigosJogo jogo ++ novosInimigos, portaisJogo = atualizaPortal portal } 
+  where 
     novosInimigos = take 1 (inimigosOnda onda) -- Ativa apenas o próximo inimigo 
     ondaAtualizada = onda {inimigosOnda = drop 1 (inimigosOnda onda)} 
     atualizaPortal p = [p {ondasPortal = ondaAtualizada : ondas}]
-
-  -- Tarefa 2 concluída --
